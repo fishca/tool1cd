6191fa947c4385886a8cb1665235c32aec3e0536
fishca <sergrud@mail.ru>
2017-11-16 10:31:25 +0300

    Испр2

--- src/SystemClasses/TFileStream.cpp
+++ src/SystemClasses/TFileStream.cpp
@@ -60,7 +60,7 @@ void TFileStream::Close()
 	_stream.reset();
 }
 
-size_t TFileStream::Read(void *Buffer, size_t Count)
+int64_t TFileStream::Read(void *Buffer, int64_t Count)
 {
 	Reopen();
 	return TWrapperStream::Read(Buffer, Count);
